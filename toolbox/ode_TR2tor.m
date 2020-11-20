3e7b7af5577ae62303ef66550cc000a868538361
Mingwu Li <limw0601@gmail.com>
2020-11-20 18:48:30 +0100

    add setting to TR2tor and plot_torus

--- toolbox/ode_TR2tor.m
+++ toolbox/ode_TR2tor.m
@@ -23,12 +23,14 @@ function prob = ode_TR2tor(prob, oid, varargin)
 
 % Copyright (C) MINGWU LI
 
-grammar   = 'RUN [SOID] LAB [NSEG]';
+grammar   = 'RUN [SOID] LAB [NSEG] [ROT [EPS]]';
 args_spec = {
      'RUN', 'cell', '{str}',  'run',  {}, 'read', {}
     'SOID',     '',   'str', 'soid', oid, 'read', {}
      'LAB',     '',   'num',  'lab',  [], 'read', {}
      'NSEG',    '',   'num',  'nseg', [], 'read', {}
+     'ROT',     '',   'str',  'rot',  [], 'read', {}
+     'EPS',     '',   'num',  'eps',  [], 'read', {}
   };
 opts_spec = [];
 [args, ~] = coco_parse(grammar, args_spec, opts_spec, varargin{:});
@@ -37,6 +39,11 @@ if isempty(args.nseg)
     N = 10;
 else
     N = args.nseg;
+end
+if isempty(args.eps)
+    epsilon = 1e-4;
+else
+    epsilon = args.eps;
 end
 
 % recover trajectory of variational eqs
@@ -49,29 +56,40 @@ M0    = fdata.coll_tst.M(fdata.coll_tst.M0_idx,:);
 M     = M1/M0;
 [vs,d] = eig(M);
 d      = diag(d);
+assert(~isreal(d), 'no complex conjugate eigenvalues at TR point');
+dabs   = abs(d);
+idunit = find(abs(dabs-1)<1e-4);
+assert(~isempty(idunit), 'no eigenvalues with unit modulus');
+% remove non-resonant modes
+d  = d(idunit);
+vs = vs(:,idunit);
 
 % extract v, a and b from sol.var
 [sol, ~] = po_read_solution(args.soid, args.run, args.lab);
-v = sol.var.v;
+% v = sol.var.v;
 a = sol.tr.u0(1);
 b = sol.tr.u0(2);
 assert(min(abs(d-(a+1j*b)))<1e-3*abs(a+1j*b),...
     'stored TR solution has inconsistency');
-if size(v,2)==2
-    vR = v(:,1);
-    vI = v(:,2);
-else
+% if size(v,2)==2
+%     vR = v(:,1);
+%     vI = v(:,2);
+% else
     idx = find(imag(d)>0);
-    vR  = real(vs(:,idx));
+    vR = real(vs(:,idx));
     vI = imag(vs(:,idx));
     th = atan2(2*vR'*vI,vI'*vI-vR'*vR);
     vr = exp(0.5*th*1j)*(vR+1j*vI); % rotated eigenvector
     vR = real(vr);
     vI = imag(vr);
-    if abs(a+1j*b-d(idx))>1e-3*abs(a+1j*b)
+    if abs(a+1j*b-d(idx))>1e-3*abs(a+1j*b) % can be simplified
         a = real(d(idx));
         b = imag(d(idx));
     end
+% end
+if strcmp(args.rot, 'neg')
+    vI = -vI;
+    b = -b;
 end
 
 alpha = atan2(b,a);
@@ -104,17 +122,22 @@ Ivt  = imag(vt);
 x0 = zeros(numel(t0),dim,2*N+1);
 for i=1:2*N+1
     xi = repmat(cth1(:,i),[1,dim]).*Rvt-repmat(sth1(:,i),[1,dim]).*Ivt;
-    xi = 1e-4*xi+xbp; % perturbed + mean
+    xi = epsilon*xi+xbp; % perturbed + mean
     x0(:,:,i) = xi;
 end
 
-figure;
-xinit = x0(1,:,:);
-xinit = reshape(xinit,[dim,2*N+1]);
-plot3(xinit(1,:),xinit(2,:),xinit(3,:),'ro'); hold on
-xfinl = x0(end,:,:);
-xfinl = reshape(xfinl,[dim,2*N+1]);
-plot3(xfinl(1,:),xfinl(2,:),xfinl(3,:),'bs'); hold on
+% figure;
+% xinit = x0(1,:,:);
+% xinit = reshape(xinit,[dim,2*N+1]);
+% xfinl = x0(end,:,:);
+% xfinl = reshape(xfinl,[dim,2*N+1]);
+% if dim>2
+%     plot3(xinit(1,:),xinit(2,:),xinit(3,:),'ro'); hold on
+%     plot3(xfinl(1,:),xfinl(2,:),xfinl(3,:),'bs'); hold on
+% else
+%     plot(xinit(1,:),xinit(2,:),'ro'); hold on
+%     plot(xfinl(1,:),xfinl(2,:),'bs'); hold on
+% end
 
 p0 = sol.p;
 
--- toolbox/plot_torus.m
+++ toolbox/plot_torus.m
@@ -1,51 +1,61 @@
-function plot_torus(toid, run, lab, fac)
+function plot_torus(toid, run, lab, outdof, varargin)
 % Plot torus
 % toid - the instance of torus, a typical used one is ''
 % run  - the id of run
 % lab  - the label of solution
 % fac  - fraction of plot
 
-if nargin<4
-  fac=0.75;
+if nargin<5
+    fac= 1;
+else
+    fac = varargin{1};
 end
+
+ndof = numel(outdof);
+assert(ndof<4, 'visualization of 4 or higher dimensional torus is not supported');
+assert(ndof>1, 'the dimension of outdof should be larger than one');
 
 [sol, ~] = tor_read_solution(toid, run, lab);
 tube = sol.xbp; % [nt dim nsegs+1]
-[nt,dim,nsegs] = size(tube);
+[nt,~,nsegs] = size(tube);
 M    = ceil(nt*fac);
 tube = permute(tube, [1,3,2]);
 tube = tube(1:M,:,:); % a fraction of torus
-if dim<3
+if ndof<3
     % plot of x1-t-x2
     tbp = repmat(sol.tbp, [1,nsegs]);
     tbp = tbp(1:M,:);
     figure; hold on
-    surf(tube(:,:,1), tbp, tube(:,:,2), 'FaceColor', 0.9*[1 1 1], 'FaceAlpha', 0.7, ...
+    surf(tube(:,:,outdof(1)), tbp, tube(:,:,outdof(2)), 'FaceColor', 0.9*[1 1 1], 'FaceAlpha', 0.7, ...
         'MeshStyle', 'column', 'LineStyle', '-', 'EdgeColor', 0.6*[1 1 1], ...
         'LineWidth', 0.5);
-    plot3(tube(1,:,1), zeros(nsegs,1), tube(1,:,2), 'LineStyle', '-', 'LineWidth', 2, ...
+    plot3(tube(1,:,outdof(1)), zeros(nsegs,1), tube(1,:,outdof(2)), 'LineStyle', '-', 'LineWidth', 2, ...
         'Color', 'black', 'Marker', '.', 'MarkerSize', 12);
-    plot3(tube(end,:,1), tbp(end)*ones(nsegs,1), tube(end,:,2), 'LineStyle', '-', 'LineWidth', 2, ...
+    plot3(tube(end,:,outdof(1)), tbp(end)*ones(nsegs,1), tube(end,:,outdof(2)), 'LineStyle', '-', 'LineWidth', 2, ...
         'Color', 'blue', 'Marker', '.', 'MarkerSize', 12);
     view([50 15]); grid on
-    xlabel('$x_1$','interpreter','latex','FontSize',14);
+    xlabel(['$x_\mathrm{',num2str(outdof(1)),'}$'],'interpreter','latex','FontSize',14);
     ylabel('$t$','interpreter','latex','FontSize',14);
-    zlabel('$x_2$','interpreter','latex','FontSize',14);
+    zlabel(['$x_\mathrm{',num2str(outdof(2)),'}$'],'interpreter','latex','FontSize',14);
+    title(['$\omega_1=',num2str(sol.p(end-2)),', \omega_2=',num2str(sol.p(end-1)),'$'],...
+        'interpreter','latex','FontSize',14);
     hold off
 else
     % plot of x1-x2-x3
     figure; hold on
-    surf(tube(:,:,1), tube(:,:,2), tube(:,:,3), 'FaceColor', 0.9*[1 1 1], 'FaceAlpha', 0.7, ...
+    surf(tube(:,:,outdof(1)), tube(:,:,outdof(2)), tube(:,:,outdof(3)), 'FaceColor', 0.9*[1 1 1], 'FaceAlpha', 0.7, ...
         'MeshStyle', 'column', 'LineStyle', '-', 'EdgeColor', 0.6*[1 1 1], ...
         'LineWidth', 0.5);
-    plot3(tube(1,:,1), tube(1,:,2), tube(1,:,3), 'LineStyle', '-', 'LineWidth', 2, ...
+    plot3(tube(1,:,outdof(1)), tube(1,:,outdof(2)), tube(1,:,outdof(3)), 'LineStyle', '-', 'LineWidth', 2, ...
         'Color', 'black', 'Marker', '.', 'MarkerSize', 12);
-    plot3(tube(end,:,1), tube(end,:,2), tube(end,:,3), 'LineStyle', '-', 'LineWidth', 2, ...
+    plot3(tube(end,:,outdof(1)), tube(end,:,outdof(2)), tube(end,:,outdof(3)), 'LineStyle', '-', 'LineWidth', 2, ...
         'Color', 'blue', 'Marker', '.', 'MarkerSize', 12);
     view([50 15]); grid on
-    xlabel('$x_1$','interpreter','latex','FontSize',14);
-    ylabel('$x_2$','interpreter','latex','FontSize',14);
-    zlabel('$x_3$','interpreter','latex','FontSize',14);
+    xlabel(['$x_\mathrm{',num2str(outdof(1)),'}$'],'interpreter','latex','FontSize',14);
+    ylabel(['$x_\mathrm{',num2str(outdof(2)),'}$'],'interpreter','latex','FontSize',14);
+    zlabel(['$x_\mathrm{',num2str(outdof(3)),'}$'],'interpreter','latex','FontSize',14);
+    title(['$\omega_1=',num2str(sol.p(end-2)),', \omega_2=',num2str(sol.p(end-1)),'$'],...
+        'interpreter','latex','FontSize',14);
     hold off
 end
 
