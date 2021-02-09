f40b8d7e5f8ba773559fe8c3b7cab9944017f08f
Mingwu Li <limw0601@gmail.com>
2021-02-09 15:44:57 +0100

    minor edits and add tor-interpolation routine

--- toolbox/tor_interpolation.m
+++ toolbox/tor_interpolation.m
@@ -1,16 +1,16 @@
-function y = tor_amplitude(x)
-% TOR_AMPLITUDE This function recovers the torus solution based on Fourier
-% expansion and then calcualte the amplitude of this torus based on L_infty
-% norm.
+function y = tor_interpolation(x,angs)
+% TOR_INTERPOLATION This function recovers the torus solution based on Fourier
+% expansion and then return torus solution with (high) fidelty at angs
 %
-% Y = TOR_AMPLITUDE(X)
+% Y = TOR_INTERPOLATION(X)
 %
 % x: a collection of trajectories at (j-1)*2pi/(2N+1), j=0,...,2N+1. x should
 %    have the size nt-dim-(2N+2), where nt is the number of time points and dim
 %    is the dimension of state x
+% angs: interpolated solution at these angles
 % y: dim dimensional amplitudes of state vector x
 
-[~,dim,nsegs] = size(x);
+[~,~,nsegs] = size(x);
 % construct mapping matrix F
 N  = nsegs/2-1;
 Th = 2*pi*(0:2*N)/(2*N+1);
@@ -18,13 +18,7 @@ Th = kron(1:N, Th');
 F  = [ones(2*N+1,1) 2*reshape([cos(Th);sin(Th)], [2*N+1 2*N])]'/(2*N+1);
 x  = permute(x, [3,1,2]);
 % Fourier expansion to recover
-angs  = linspace(0,2*pi,129); 
-tube  = Fourier_recover(x(1:end-1,:,:), F, angs); % 129-nt-dim
-
-y = zeros(dim,1);
-for i=1:dim
-    xi = tube(:,:,i);
-    y(i) = norm(xi(:),'inf');
-end
+y  = Fourier_recover(x(1:end-1,:,:), F, angs); % num_angs-nt-dim
+y  = permute(y, [2,3,1]);
 
 end
